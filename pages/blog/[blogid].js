909c10e6bcfc7deb75b23a4159dbebdd19d6596e
hitarthi <hppatel0405@gmail.com>
2022-02-14 11:51:28 +0530

    bloggg

--- pages/blog/[blogid].js
+++ pages/blog/[blogid].js
@@ -1,4 +1,4 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { db, auth } from "../../firebase";
 import { useRouter } from "next/router";
@@ -36,16 +36,81 @@ export default function Blogpage({ blogg, user, allComments }) {
 
   const users = auth.currentUser;
   //const { blogid } = router.query.Blogid;
- 
 
-  console.log(   "blog", blogg);
-  console.log(   "comments", allComments);
+  console.log("blog", blogg);
+  console.log("comments", allComments);
 
-  
+  const makeCommet = async () => {
+    const docData = {
+      text: myComment,
+      name: users.uid,
+    };
+    const docRef = doc(db, "blogs", `${router.query.blogid}`);
+    //const docuref =collection(db, "blogs",`${blogid}`,"comments");
+    const cmmm = await setDoc(doc(docRef, "comments", `${uuidv4()}`), docData);
+
+    const cnn = collection(db, "blogs", `${router.query.blogid}`, "comments");
+    const getSnap = await getDocs(cnn);
+
+    const allCommentsB = [];
+    getSnap.forEach((doc) => {
+      allCommentsB.push({ ...doc.data(), id: doc.id });
+    });
+    setAllComments(allCommentsB);
+  };
+
   return (
     <div className="center">
       <h1> ui rendet</h1>
-    
+      {blogg.map((blogg) => {
+        return (
+          <div className="card " key={blogg.id}>
+            <span className="card-title    orange-text  #e65100-text text-darken-4 ">
+              <div className="row">
+                <div className="col s3">
+                  {new Date(blogg.createdAt).toDateString()}
+                </div>
+                <div className="col s6"> {blogg.titleb}</div>
+              </div>
+            </span>
+           
+
+            <p className="card-content black-text "> {blogg.bodyb}</p>
+          </div>
+        );
+      })}
+
+      {user ? (
+        <>
+          <div className="input-field">
+            <input
+              type="text"
+              placeholder="add a comment"
+              value={myComment}
+              onChange={(e) => setMyComment(e.target.value)}
+            />
+          </div>
+          <button
+            className="btn #fb8c00 orange darken-1"
+            onClick={() => makeCommet()}
+          >
+            Make comment
+          </button>
+        </>
+      ) : (
+        <h3>please login to make comments</h3>
+      )}
+
+      <hr />
+      <div className="left-align">
+        {allCommentsBlog.map((item) => {
+          return (
+            <h6 key={item.name}>
+              <span>{item.name}</span> {item.text}
+            </h6>
+          );
+        })}
+      </div>
 
       <style jsx global>
         {`
